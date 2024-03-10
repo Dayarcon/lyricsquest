8d469c511a12d0c09d534aed312926eea1239f18
Dayarcon <97634034+Dayarcon@users.noreply.github.com>
2024-03-10 14:10:24 +0530

    created new react project on lyric finding website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [artist, setArtist]= useState("");
+  const [song, setTitle]= useState("");
+  const [lyrics, setLyrics]= useState("");
+  function searchLyrics () {
+    if (artist === "" || song === "") 
+    return;
+  }
+    axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`,
+    { validateStatus: function (status) {
+      return status < 500; // Resolve only if the status code is less than 500
+    }
+  }
+  )
+  
+ .then(res => { 
+  console.log(res.data.lyrics);
+  setLyrics(res.data.lyrics);
+  })
+return(
+  <div className="App">
+    <h1>Lyrics Quest</h1>
+    <input className='int' placeholder="Artist name" type="text" onChange={(e) =>{ setArtist(e.target.value) }} />
+    <input className='int' placeholder="Song name" type="text" onChange={(e) =>{ setTitle(e.target.value) }} />
+    <button className='btn' onClick={() =>searchLyrics()}>Search</button>
+    <hr />
+    <pre> {lyrics} </pre>
     </div>
-  );
-}
-
+);
+};
 export default App;
